e6eb6864b3b87157a68cd35b3a9b1834d6c3cd4b
AmayaJuan <jpva360@gmail.com>
2019-06-01 00:27:15 -0500

    ggf
    hh

---
+++ Juegos Plataforma 2D/Assets/Platform2D/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2f;
+
+    private float maxSpeed = 5f;
+
+    Rigidbody2D rb2D;
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rb2D.AddForce(Vector2.right * speed * h);
+
+        if (rb2D.velocity.x > maxSpeed)
+            rb2D.velocity = new Vector2(maxSpeed, rb2D.velocity.x);
+
+        if (rb2D.velocity.x < -maxSpeed)
+            rb2D.velocity = new Vector2(-maxSpeed, rb2D.velocity.x);
+    }
+}
