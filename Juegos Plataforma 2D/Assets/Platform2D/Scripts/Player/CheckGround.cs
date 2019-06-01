3b4e702040154eccfd07077ac6c019c849bfa798
AmayaJuan <jpva360@gmail.com>
2019-06-01 17:19:00 -0500

    jjj
    jj

---
+++ Juegos Plataforma 2D/Assets/Platform2D/Scripts/Player/CheckGround.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<PlayerController>();
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            player.grounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            player.grounded = false;
+    }
+}
--- Juegos Plataforma 2D/Assets/Platform2D/Scripts/Player/PlayerController.cs
+++ Juegos Plataforma 2D/Assets/Platform2D/Scripts/Player/PlayerController.cs
@@ -2,15 +2,20 @@
 
 public class PlayerController : MonoBehaviour
 {
+    [HideInInspector]public bool grounded;
+    public float maxSpeed = 5f;
     public float speed = 2f;
+    public float jumpPower = 6.5f;
 
-    private float maxSpeed = 5f;
+    private bool jump;
 
+    Animator anim;
     Rigidbody2D rb2D;
 
     void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -22,19 +27,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetFloat("Speed", Mathf.Abs(rb2D.velocity.x));
+        anim.SetBool("Grounded", grounded);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+            jump = true;
     }
 
     void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
-
         rb2D.AddForce(Vector2.right * speed * h);
+        float limitedSpeed = Mathf.Clamp(rb2D.velocity.x, -maxSpeed, maxSpeed);
+        rb2D.velocity = new Vector2(limitedSpeed, rb2D.velocity.y);
 
-        if (rb2D.velocity.x > maxSpeed)
-            rb2D.velocity = new Vector2(maxSpeed, rb2D.velocity.x);
+        if (h > .1f)
+            transform.localScale = new Vector3(1f, 1f, 1f);
 
-        if (rb2D.velocity.x < -maxSpeed)
-            rb2D.velocity = new Vector2(-maxSpeed, rb2D.velocity.x);
+        if (h < -.1f)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        if (jump)
+        {
+            rb2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jump = false;
+        }
     }
 }
